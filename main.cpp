7b24ecbd3e9089858697e979bbe50b03e19a1a9e
AndreyMustafin2001 <andrey.must2001@gmail.com>
2023-12-06 01:09:57 +0400

    messagelength
    добавлен блок который заполняет сообщение сначала 1, а потом нулями пока последний кусок не достигнет длины 448

--- main.cpp
+++ main.cpp
@@ -20,13 +20,28 @@ std::string sha1(std::string mess)
             message.push_back(mess[i]&(1<<j));// Adding j-th bit to the vector
         }
     }
-}
+
     unsigned int H0 = 0x67452301;
     unsigned int H1 = 0xEFCDAB89;
     unsigned int H2 = 0x98BADCFE;
     unsigned int H3 = 0x10325476;
     unsigned int H4 = 0xC3D2E1F0;
 
+    long long message_length = message.size();// long long because of length is 64 bits
+    bool flag = true;
+    while(message.size() % 512 != 448)//Adding 1 and 0s until the last chunk is of the length 448
+    {
+        if (flag == true)
+        {
+           message.push_back(1);// First adding 1
+           flag = false;
+        }
+        else
+        {
+          message.push_back(0);// Then adding only 0s
+        }
+    }
+}
 int main()
 {
 
